0aaaaa8c3188e59749a64ac7799554e228eea2b1
hanbaek <hanbaek@kakao.com>
2020-10-13 19:23:18 +0900

    :label:Modify button behavior

--- app/src/main/java/com/example/outstagram2/SignupemailActivity.kt
+++ app/src/main/java/com/example/outstagram2/SignupemailActivity.kt
@@ -1,26 +1,64 @@
 package com.example.outstagram2
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_signupemail.*
 
 class SignupemailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signupemail)
+        et_user_name.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) { // 입력이 끝날때 실행되는 함수
+                if (et_user_name.text.toString() == "") {
+                    btn_next_mail.setEnabled(false)
+                    btn_next_mail.setBackgroundResource(R.drawable.button_colorchange)
+                    //btn_next_mail.setBackground(ContextCompat.getDrawable(this, R.drawable.button_colorchange))
+                    //btn_next_mail.layout(R.drawable.button_colorchange)
+                    //btn_next_mail.setBackgroundColor(R.drawable.button_colorchange)
+                } else {
+                    btn_next_mail.setEnabled(true)
+                    btn_next_mail.setBackgroundResource(R.drawable.button_colorchange2)
+                    //btn_next_mail.setBackground(ContextCompat.getDrawable(this, R.drawable.button_colorchange2))
+                    //btn_next_mail.layout(R.drawable.button_colorchange2)
+                    //btn_next_mail.setBackgroundColor(R.drawable.button_colorchange2)
 
-
-
-        btn_next_mail.setOnClickListener {
-            if (et_user_name.text.toString() == "") {
-                Toast.makeText(this, "이름을 입력해주세요.", Toast.LENGTH_SHORT).show()
-            } else {
-                val intentToPassword = Intent(this, SignuppasswordActivity::class.java)
-                intentToPassword.putExtra("nameValue", et_user_name.text.toString())
-                startActivity(intentToPassword)
+                }
             }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { // 입력하기 전에 실행되는 함수
+                btn_next_mail.setEnabled(false)
+                //btn_next_mail.setBackgroundColor(R.drawable.button_colorchange2)
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) { // 타이핑 되는 텍스트에 변화가 있을때
+                //btn_next_mail.setBackgroundColor(R.drawable.button_colorchange2)
+                if (et_user_name.text.toString() == "") {
+                    btn_next_mail.setEnabled(false)
+                    btn_next_mail.setBackgroundResource(R.drawable.button_colorchange)
+                    //btn_next_mail.setBackground(ContextCompat.getDrawable(this, R.drawable.button_colorchange))
+
+                    //btn_next_mail.layout(R.drawable.button_colorchange)
+                    //btn_next_mail.setBackgroundColor(R.drawable.button_colorchange)
+                } else {
+                    btn_next_mail.setEnabled(true)
+                    btn_next_mail.setBackgroundResource(R.drawable.button_colorchange2)
+                    //btn_next_mail.setBackground(ContextCompat.getDrawable(this, R.drawable.button_colorchange2))
+                    //btn_next_mail.layout(R.drawable.button_colorchange2)
+                    //btn_next_mail.setBackgroundColor(R.drawable.button_colorchange2)
+                }
+                }
+        })
+        btn_next_mail.setOnClickListener {
+            val intentToPassword = Intent(this, SignuppasswordActivity::class.java)
+            intentToPassword.putExtra("nameValue", et_user_name.text.toString())
+            startActivity(intentToPassword)
         }
     }
 }
--- app/src/main/java/com/example/outstagram2/SignuppasswordActivity.kt
+++ app/src/main/java/com/example/outstagram2/SignuppasswordActivity.kt
@@ -3,6 +3,8 @@ package com.example.outstagram2
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_signupemail.*
 import kotlinx.android.synthetic.main.activity_signuppassword.*
@@ -12,17 +14,62 @@ class SignuppasswordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signuppassword)
+        et_user_password.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) { // 입력이 끝날때 실행되는 함수
+                if (et_user_password.text.toString() == "") {
+                    btn_next_password.setEnabled(false)
+                    btn_next_password.setBackgroundResource(R.drawable.button_colorchange)
+                    //btn_next_mail.setBackground(ContextCompat.getDrawable(this, R.drawable.button_colorchange))
+                    //btn_next_mail.layout(R.drawable.button_colorchange)
+                    //btn_next_mail.setBackgroundColor(R.drawable.button_colorchange)
+                } else {
+                    btn_next_password.setEnabled(true)
+                    btn_next_password.setBackgroundResource(R.drawable.button_colorchange2)
+                    //btn_next_mail.setBackground(ContextCompat.getDrawable(this, R.drawable.button_colorchange2))
+                    //btn_next_mail.layout(R.drawable.button_colorchange2)
+                    //btn_next_mail.setBackgroundColor(R.drawable.button_colorchange2)
+
+                }
+            }
+            override fun beforeTextChanged(
+                s: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
+            ) { // 입력하기 전에 실행되는 함수
+                btn_next_password.setEnabled(false)
+                //btn_next_mail.setBackgroundColor(R.drawable.button_colorchange2)
+            }
+
+            override fun onTextChanged(
+                s: CharSequence?,
+                start: Int,
+                before: Int,
+                count: Int
+            ) { // 타이핑 되는 텍스트에 변화가 있을때
+                //btn_next_mail.setBackgroundColor(R.drawable.button_colorchange2)
+                if (et_user_password.text.toString() == "") {
+                    btn_next_password.setEnabled(false)
+                    btn_next_password.setBackgroundResource(R.drawable.button_colorchange)
+                    //btn_next_mail.setBackground(ContextCompat.getDrawable(this, R.drawable.button_colorchange))
+                    //btn_next_mail.layout(R.drawable.button_colorchange)
+                    //btn_next_mail.setBackgroundColor(R.drawable.button_colorchange)
+                } else {
+                    btn_next_password.setEnabled(true)
+                    btn_next_password.setBackgroundResource(R.drawable.button_colorchange2)
+                    //btn_next_mail.setBackground(ContextCompat.getDrawable(this, R.drawable.button_colorchange2))
+                    //btn_next_mail.layout(R.drawable.button_colorchange2)
+                    //btn_next_mail.setBackgroundColor(R.drawable.button_colorchange2)
+                }
+            }
+        })
 
         btn_next_password.setOnClickListener {
-            if (et_user_password.text.toString() == "") {
-                Toast.makeText(this, "비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show()
-            } else {
-                val intentToWelcome = Intent(this, WelcomeActivity::class.java)
-                if (intent.hasExtra("nameValue")) {
-                    intentToWelcome.putExtra("nameValue", intent.getStringExtra("nameValue"))
-                }
-                startActivity(intentToWelcome)
+            val intentToWelcome = Intent(this, WelcomeActivity::class.java)
+            if (intent.hasExtra("nameValue")) {
+                intentToWelcome.putExtra("nameValue", intent.getStringExtra("nameValue"))
             }
+            startActivity(intentToWelcome)
         }
 
     }
